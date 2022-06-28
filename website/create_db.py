d29be061e162ba5348fdf05118f292f482781085
Tarcísio Souza de Melo <dev.engtsm@gmail.com>
2022-06-28 10:35:31 -0300

    =( 9

--- website/create_db.py
+++ website/create_db.py
@@ -1,6 +1,25 @@
-import mysql.connector
+# pode haver algum erro aqui
 
-mydb = mysql.connector.connect(
+# import mysql.connector
+
+# mydb = mysql.connector.connect(
+#     host= "localhost",
+#     user="root",
+#     passwd="123",
+
+# )
+# my_cursor = mydb.cursor()
+
+# my_cursor.execute("CREATE DATABASE tabloid")
+
+# my_cursor.execute("SHOW DATABASES")
+
+# for db in my_cursor:
+#     print(db)
+
+import psycopg2
+
+mydb = psycopg2.connect(
     host= "localhost",
     user="root",
     passwd="123",
